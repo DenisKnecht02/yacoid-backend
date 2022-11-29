d6afd8231808d2d82b72bc8bbe178c7788670123
Denis Knecht <denis.knecht@mail.de>
2022-11-29 16:54:42 +0100

    feat: added cors policy

--- api/api.go
+++ api/api.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func StartAPI() {
@@ -23,6 +24,12 @@ func StartAPI() {
 	app := fiber.New()
 
 	api := app.Group("/api")
+
+	api.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
+
 	v1 := api.Group("/v1")
 
 	validate := validator.New()
